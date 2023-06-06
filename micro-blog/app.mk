8424076fe64883cf1a61a913f4dbf945953f153f
devexps <dev.experiences@gmail.com>
2023-06-06 14:04:37 +0700

    refactor: app.mk & api third_party

--- micro-blog/app.mk
+++ micro-blog/app.mk
@@ -4,21 +4,6 @@ GOPATH ?= $(shell go env GOPATH)
 ifeq "$(GOPATH)" ""
   $(error Please set the environment variable GOPATH before running `make`)
 endif
-FAIL_ON_STDOUT := awk '{ print } END { if (NR > 0) { exit 1 } }'
-
-GO			:= GO111MODULE=on go
-
-ARCH		:= "`uname -s`"
-LINUX		:= "Linux"
-MAC			:= "Darwin"
-
-ifeq ($(OS),Windows_NT)
-    IS_WINDOWS:=1
-endif
-
-APP_VERSION=$(shell git describe --tags --always)
-APP_RELATIVE_PATH=$(shell a=`basename $$PWD` && cd .. && b=`basename $$PWD` && echo $$b/$$a)
-APP_NAME=$(shell echo $(APP_RELATIVE_PATH) | sed -En "s/\//-/p")
 
 CURRENT_DIR=$(shell a=`pwd` && echo $$a/)
 API_PROTO=../api/proto/
